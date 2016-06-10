01b0e72841049c4ded9143b04fb913deff873408
Zhang Li <xiaoxin88214@163.com>
2016-06-10 10:16:12 +0800

    init

---
+++ funniest/__init__.py
@@ -0,0 +1,8 @@
+from .text import joke
+from markdown import markdown
+
+
+def joke2():
+    return markdown(u'Wenn ist das Nunst\u00fcck git und Slotermeyer?'
+                    u'Ja! ... **Beiherhund** das Oder die Flipperwaldt '
+                    u'gersput.')
---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(name='funniest',
+      version='0.1',
+      description='The funniest joke in the world',
+      long_description=readme(),
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Text Processing :: Linguistic',
+      ],
+      keywords='funniest joke comedy flying circus',
+      url='http://github.com/storborg/funniest',
+      author='Flying Circus',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      packages=['funniest'],
+      install_requires=[
+          'markdown',
+      ],
+      include_package_data=True,
+      zip_safe=False)
